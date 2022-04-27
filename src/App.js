9f2dd598f111361a18fc415c205147c833b3bd9f
morisdev <moris.dmitry@gmail.com>
2022-04-27 14:47:27 +0300

    add create comix page component and made drag functions for images

--- src/App.js
+++ src/App.js
@@ -1,22 +1,92 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import CreateComixPage from './components/create_comix_page/createComixPage';
 
 function App() {
+  const [left, setLeft] = useState(500);
+  const [top, setTop] = useState(100);
+  const [contact, setContact] = useState('off')
+
+
+  console.log('contact', contact)
+
+  function moveAt(pageX, pageY) {
+    setLeft(pageX - 40 / 2)
+    setTop(pageY - 40 / 2)
+  }
+
+  function onMouseMove(event){
+    if (contact === 'on'){
+      moveAt(event.pageX, event.pageY);
+    }
+  };
+
+
+  function onMouseDown(event){
+
+    if (contact === 'on'){
+      moveAt(event.pageX, event.pageY);
+      setContact('off')
+    }
+    if (contact === 'off'){
+      moveAt(event.pageX, event.pageY);
+      setContact('on')
+    }
+  
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-main">
+        <div className="main-content">
+
+          <CreateComixPage />
+
+        </div>
+  {/* <img 
+  id='ball' 
+  onMouseDown={onMouseDown}
+  onMouseMove={onMouseMove}
+  src={'https://js.cx/clipart/ball.svg'} 
+  style={{color: "#FF8C00", cursor: "pointer", position: "absolute", z_index: "1000", left: `${left}px`, top: `${top}px`}}
+  
+  /> */}
+
+
+
+
+
+
+
+      
+      
+      
+      
+      
+      
       </header>
     </div>
   );
---
+++ src/components/create_comix_page/createComixPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import StrFields  from '../strFields/strFields';
+import Buttons  from '../buttons/buttons';
+
+class CreateComixPage extends React.Component {
+    render() {
+        return (
+            <div>
+
+                <StrFields></StrFields>
+                <Buttons></Buttons>
+            </div>
+        )
+    }
+}
+
+export default CreateComixPage;
